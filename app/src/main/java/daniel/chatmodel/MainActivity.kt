fadca857dbd897bd918afef97e16a0ab5eb54a88
DanielJorn <0954264@gmail.com>
2021-02-07 10:18:27 +0200

    Initial commit

---
+++ app/src/main/java/daniel/chatmodel/MainActivity.kt
@@ -0,0 +1,13 @@
+package daniel.chatmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
