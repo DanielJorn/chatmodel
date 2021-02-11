7d421d4aacc34cf214a22a1de321d375dd3a5094
DanielJorn <0954264@gmail.com>
2021-02-11 13:29:19 +0200

    Changed db to firestore

--- app/src/main/java/daniel/chatmodel/features/chats/ChatListFragment.kt
+++ app/src/main/java/daniel/chatmodel/features/chats/ChatListFragment.kt
@@ -15,25 +15,11 @@ class ChatListFragment: Fragment(R.layout.fragment_chat_list){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-       /* val chat1 = ChatPreviewModel("Chat1", "Some message")
-        val chat2 = ChatPreviewModel("Chat12", "asdkap message")
-        val chat3 = ChatPreviewModel("Chat13", "He needs some milk")*/
-
         viewModel.updateLiveData.observe(viewLifecycleOwner) { onChatListUpdated() }
-
 
         adapter = ChatPreviewAdapter(viewModel.chatList)
         viewModel.updateChatList()
         rv_chat_list.adapter=adapter
-
-        /*
-
-        fakeList.add(chat1)
-        fakeList.add(chat2)
-        fakeList.add(chat3)
-
-        adapter.notifyDataSetChanged()*/
-
     }
 
     private fun onChatListUpdated() {
--- app/src/main/java/daniel/chatmodel/features/chats/ChatListViewModel.kt
+++ app/src/main/java/daniel/chatmodel/features/chats/ChatListViewModel.kt
@@ -1,71 +1,73 @@
 package daniel.chatmodel.features.chats
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 private const val TAG = "ChatListViewModel"
 
-class ChatListViewModel: ViewModel(){
+class ChatListViewModel : ViewModel() {
     val chatList = ArrayList<ChatPreviewModel>()
-    /*private val mutableChatList = MutableLiveData<List<ChatPreviewModel>>()
-    val chatListLiveData: LiveData<List<ChatPreviewModel>> = mutableChatList*/
     private val mutableUpdateLiveData = MutableLiveData<Unit>()
-    val updateLiveData : LiveData<Unit> = mutableUpdateLiveData
+    val updateLiveData: LiveData<Unit> = mutableUpdateLiveData
 
     private val errorLiveData = MutableLiveData<String>()
     val error: LiveData<String> = errorLiveData
 
-    private val database = Firebase.database
-    private val chatsRef = database.getReference("chats")
+    private val database = Firebase.firestore
 
-    fun updateChatList(){
-        chatsRef.addChildEventListener(object: ChildEventListener{
-            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                val chatPreview = snapshot.getValue(ChatPreviewModel::class.java)!!
-                Log.d(TAG, "onChildAdded: $chatPreview")
-
-                chatList.add(chatPreview)
-                mutableUpdateLiveData.value = mutableUpdateLiveData.value
-            }
-
-            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
-                val changedChatModel = snapshot.getValue(ChatPreviewModel::class.java)!!
-                Log.d(TAG, "onChildChanged: chat entity: $changedChatModel")
-
-                chatList.forEachIndexed { currInd, currChatModel ->
-                    val nullPos = -1
-                    var indToChange = nullPos
-
-                    if(currChatModel.id == changedChatModel.id)
-                        indToChange = currInd
-                    if(indToChange != nullPos)
-                        chatList[indToChange] = changedChatModel
+    fun updateChatList() {
+        database.collection("chats")
+            .addSnapshotListener { snapshot, e ->
+                if (e != null) {
+                    handleError();
+                    return@addSnapshotListener
                 }
-                mutableUpdateLiveData.value = mutableUpdateLiveData.value
-            }
 
-            override fun onCancelled(error: DatabaseError) {
-                errorLiveData.value = error.message
-                Log.d(TAG, "onCancelled: error msg is: ${error.message}")
-                Log.d(TAG, "onCancelled: error details are: ${error.details}")
-            }
+                snapshot?.documentChanges?.forEach {
+                    val chatPreview = it.document.toObject(ChatPreviewModel::class.java)
 
-            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
-                Log.d(TAG, "onChildMoved: snapshot")
+                    when (it.type) {
+                        DocumentChange.Type.ADDED -> onChatAdded(chatPreview)
+                        DocumentChange.Type.MODIFIED -> onChatModified(chatPreview)
+                        DocumentChange.Type.REMOVED -> onChatRemoved(chatPreview)
+                    }
+                }
             }
+    }
 
-            override fun onChildRemoved(snapshot: DataSnapshot) {
-                Log.d(TAG, "onChildRemoved: ")
+    private fun onChatRemoved(chatPreview: ChatPreviewModel) {
+        chatList.removeAll { it.id == chatPreview.id }
+        mutableUpdateLiveData.value = mutableUpdateLiveData.value
+
+    }
+
+    private fun onChatModified(modifiedModel: ChatPreviewModel) {
+        chatList.forEachIndexed { currInd, currChatModel ->
+            val nullPos = -1
+            var indToChange = nullPos
+
+            if (currChatModel.id == modifiedModel.id)
+                indToChange = currInd
+
+            if (indToChange != nullPos) {
+                chatList[indToChange] = modifiedModel
+                return@forEachIndexed
             }
+        }
 
-        })
+        mutableUpdateLiveData.value = mutableUpdateLiveData.value
+    }
+
+    private fun onChatAdded(chatPreview: ChatPreviewModel) {
+        chatList.add(chatPreview)
+        mutableUpdateLiveData.value = mutableUpdateLiveData.value
+    }
+
+    private fun handleError() {
 
     }
 }
