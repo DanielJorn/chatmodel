76a58b9ae1aba27ce026b6f0e50030adebd4c518
Anton <0954264@gmail.com>
2021-08-07 09:20:04 +0300

    Experementing with ChatListRepository

--- app/src/main/java/daniel/chatmodel/base/firestore/FirestoreExt.kt
+++ app/src/main/java/daniel/chatmodel/base/firestore/FirestoreExt.kt
@@ -1,12 +1,16 @@
 package daniel.chatmodel.base.firestore
 
 import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.QuerySnapshot
 
-fun Query.handleUpdates(onSuccess: (QuerySnapshot) -> Unit, onFailure: (FirebaseFirestoreException) -> Unit) {
-    addSnapshotListener { snapshot, e ->
-        if (e != null) onFailure(e)
-        else onSuccess(snapshot!!)
+fun Query.handleUpdates(onSuccess: (QuerySnapshot) -> Unit, onFailure: (Exception) -> Unit): ListenerRegistration {
+    return addSnapshotListener { snapshot, e ->
+        when {
+            e != null -> onFailure(e)
+            snapshot == null -> onFailure(Exception())
+            else -> onSuccess(snapshot)
+        }
     }
 }
--- app/src/main/java/daniel/chatmodel/features/chat/chatList/ChatListRepository.kt
+++ app/src/main/java/daniel/chatmodel/features/chat/chatList/ChatListRepository.kt
@@ -1,16 +1,16 @@
 package daniel.chatmodel.features.chat.chatList
 
 import android.util.Log
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.*
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import daniel.chatmodel.base.Failure
 import daniel.chatmodel.base.Loading
 import daniel.chatmodel.base.State
 import daniel.chatmodel.base.Success
 import daniel.chatmodel.base.firestore.CHATS
+import daniel.chatmodel.base.firestore.handleUpdates
 import daniel.chatmodel.features.chat.ChatModel
+import kotlinx.coroutines.channels.ProducerScope
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
@@ -19,41 +19,39 @@ private const val TAG = "ChatListRepository"
 
 class ChatListRepository {
     private val db = Firebase.firestore
-    private val auth = Firebase.auth
-    private val listenerList = arrayListOf<ListenerRegistration>()
+    private var mainListener: ListenerRegistration? = null
 
-    fun loadChatList(): Flow<State<List<ChatPreviewModel>>> {
-        return callbackFlow {
-            trySend(Loading)
+    private var onUpdate: (State<List<ChatPreviewModel>>) -> Unit = { }
 
-            val listener = db.collection(CHATS)
-                .addSnapshotListener { snapshot, error ->
-                    if (error != null){
-                        trySend(Failure(error))
-                        return@addSnapshotListener
-                    }
-                    if (snapshot == null){
-                        // ???
-                        return@addSnapshotListener
-                    }
-                    val dbChatList = snapshot.toObjects(ChatModel::class.java)
-                    val uiChatList = dbChatList.map { ChatPreviewModel(it.id, it.title) }
+    fun loadChatList(): Flow<State<List<ChatPreviewModel>>> = callbackFlow {
+        onUpdate = { state -> trySend(state) }
 
-                    trySend(Success(uiChatList))
-                }
+        trySend(Loading)
 
-            listenerList.add(listener)
+        val listener = db.collection(CHATS)
+            .handleUpdates(::handleNewChatList, ::handleError)
 
-            awaitClose {
-                listener.remove()
-                Log.d(TAG, "loadChatList: removed chat listener")
-            }
+        mainListener = listener
+
+        awaitClose {
+            listener.remove()
+            Log.d(TAG, "loadChatList: removed chat listener")
         }
     }
 
-    fun onCleared(){
-        listenerList.forEach {
+    private fun handleNewChatList(snapshot: QuerySnapshot) {
+        val dbChatList = snapshot.toObjects(ChatModel::class.java)
+        val uiChatList = dbChatList.map { ChatPreviewModel(it.id, it.title) }
+
+        onUpdate(Success(uiChatList))
+    }
+
+    private fun handleError(exception: Exception) = Unit
+
+    fun onCleared() {
+        mainListener?.let {
             it.remove()
+            Log.d(TAG, "onCleared: removed main listener")
         }
     }
 }
