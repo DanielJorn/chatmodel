e36947542b47c2154cbf66d7218a55cee89d3a8a
Anton <0954264@gmail.com>
2021-08-12 15:40:47 +0300

    Improved repository structure

--- app/src/main/java/daniel/chatmodel/features/chat/chatList/ChatListRepository.kt
+++ app/src/main/java/daniel/chatmodel/features/chat/chatList/ChatListRepository.kt
@@ -4,21 +4,29 @@ import android.util.Log
 import com.google.firebase.firestore.*
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import daniel.chatmodel.base.Failure
+import daniel.chatmodel.base.Loading
 import daniel.chatmodel.base.State
 import daniel.chatmodel.base.Success
 import daniel.chatmodel.base.firestore.CHATS
 import daniel.chatmodel.base.firestore.handleUpdates
 import daniel.chatmodel.features.chat.ChatModel
 import daniel.chatmodel.features.chat.MessageModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 private const val TAG = "ChatListRepository"
 
 class ChatListRepository {
+    private val messageRepository = MessageRepository()
+
     private val chatMap: MutableMap<String, ChatPreviewModel> = mutableMapOf()
-    private val listenerMap: MutableMap<String, MutableList<ListenerRegistration>> = mutableMapOf()
+    private val lastMessageJobMap: MutableMap<String, Job> = mutableMapOf()
     private var mainListener: ListenerRegistration? = null
 
     private val database = Firebase.firestore
@@ -27,7 +35,8 @@ class ChatListRepository {
     fun loadChatList(): Flow<State<List<ChatPreviewModel>>> = callbackFlow {
         onChatListUpdate = { this.trySend(it) }
 
-        mainListener = database.collection(CHATS)
+        mainListener = database
+            .collection(CHATS)
             .handleUpdates(::onChatUpdate, ::onChatUpdateFailed)
 
         awaitClose {
@@ -50,33 +59,32 @@ class ChatListRepository {
 
 
     private fun onChatAdded(chatModel: ChatModel) {
-        val listener = database.collection("chats")
-            .document(chatModel.id)
-            .collection("messages")
-            .orderBy("sent", Query.Direction.DESCENDING)
-            .limit(1)
-            .addSnapshotListener { snapshot, error ->
-                if (error != null) return@addSnapshotListener
-                if (snapshot == null) return@addSnapshotListener
-
-                onLastMessageLoaded(snapshot, chatModel)
+        val lastMessageFlow = messageRepository.listenLastMessage(chatModel.id)
+        val lastMessageJob = GlobalScope.launch {
+            lastMessageFlow.collect {
+                onLastMessageLoaded(it, chatModel)
             }
-        if (listenerMap[chatModel.id] == null) {
-            listenerMap[chatModel.id] = arrayListOf()
         }
-        listenerMap[chatModel.id]!!.add(listener)
+        lastMessageJobMap[chatModel.id] = lastMessageJob
     }
 
-    private fun onLastMessageLoaded(snapshot: QuerySnapshot, chatModel: ChatModel) {
-        if (snapshot.size() == 0) return
+    private fun onLastMessageLoaded(messageState: State<MessageModel>, chatModel: ChatModel) {
+        when (messageState) {
+            is Success -> {
+                val lastMessage = messageState.data
+                val uiChat = ChatPreviewModel(chatModel.id, chatModel.title, lastMessage.text)
 
-        val lastMessageDocument = snapshot.documents[0]
-        val lastMessage = lastMessageDocument.toObject(MessageModel::class.java) ?: return
+                chatMap[uiChat.id] = uiChat
+                onChatListUpdate(Success(chatMap.values.toList()))
+            }
+            is Failure -> {
+                Log.d(TAG, "onLastMessageLoaded: uh oh")
+            }
+            Loading -> {
+                Log.d(TAG, "onLastMessageLoaded: loading")
+            }
+        }
 
-        val uiChat = ChatPreviewModel(chatModel.id, chatModel.title, lastMessage.text)
-
-        chatMap[uiChat.id] = uiChat
-        onChatListUpdate(Success(chatMap.values.toList()))
     }
 
     private fun onChatModified(modifiedModel: ChatModel) {
@@ -95,28 +103,26 @@ class ChatListRepository {
         chatMap.remove(chatPreview.id)
 
         Log.d(TAG, "onChatRemoved: removed listeners from chat: $chatPreview")
-        listenerMap[chatPreview.id]?.forEach {
-            it.remove()
-        }
+        val lastMessageJob = lastMessageJobMap[chatPreview.id]
+        lastMessageJob?.cancel()
+
         onChatListUpdate(Success(chatMap.values.toList()))
     }
 
     private fun onChatUpdateFailed(exception: FirebaseFirestoreException) = Unit
 
-    fun onCleared(){
+    fun onCleared() {
         mainListener?.let {
             it.remove()
             Log.d(TAG, "onCleared: removed main listener")
         }
 
-        var listenerCount = 0
-        listenerMap.values.forEach {
-            it.forEach { registration ->
-                registration.remove()
-                ++listenerCount
-            }
+        var jobCount = 0
+        lastMessageJobMap.values.forEach {
+            it.cancel()
+            ++jobCount
         }
-        Log.d(TAG, "onCleared: removed $listenerCount listeners")
+        Log.d(TAG, "onCleared: canceled $jobCount jobs")
     }
 }
 
