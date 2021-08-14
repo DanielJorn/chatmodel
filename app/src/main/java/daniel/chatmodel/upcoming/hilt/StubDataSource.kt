109bee6e4eafd00130ca6309f7ef8e91422b2aad
Anton <0954264@gmail.com>
2021-08-14 07:49:58 +0300

    Basic @Provides usage

--- app/src/main/java/daniel/chatmodel/upcoming/hilt/StubDataSource.kt
+++ app/src/main/java/daniel/chatmodel/upcoming/hilt/StubDataSource.kt
@@ -1,8 +1,9 @@
 package daniel.chatmodel.upcoming.hilt
 
+import retrofit2.Retrofit
 import javax.inject.Inject
 
-class StubDataSource @Inject constructor() : HiltDataSource {
+class StubDataSource @Inject constructor(retrofit: Retrofit) : HiltDataSource {
     override fun getData(): String {
         return "stub data source"
     }
