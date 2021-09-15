fbb26865a03a41720d82f7fcfb5c3f32630d5db4
Anton <0954264@gmail.com>
2021-09-15 13:27:57 +0300

    Added dividers to recycler view

--- app/src/main/java/daniel/chatmodel/upcoming/room/presentation/RoomFragment.kt
+++ app/src/main/java/daniel/chatmodel/upcoming/room/presentation/RoomFragment.kt
@@ -7,16 +7,18 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
 import dagger.hilt.android.AndroidEntryPoint
 import daniel.chatmodel.R
 import daniel.chatmodel.databinding.FragmentRoomBinding
 import daniel.chatmodel.upcoming.room.presentation.recycleradapter.DynamicAdapter
+import kotlinx.android.synthetic.main.fragment_chat_room.*
 import kotlinx.android.synthetic.main.fragment_room.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class RoomFragment: Fragment() {
-    private val viewModel : RoomViewModel by viewModels()
+class RoomFragment : Fragment() {
+    private val viewModel: RoomViewModel by viewModels()
 
     @Inject
     lateinit var adapter: DynamicAdapter
@@ -35,6 +37,9 @@ class RoomFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         room_users_recyclerView.adapter = adapter
+        room_users_recyclerView.addItemDecoration(
+            DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
+        )
 
         viewModel.chatPreviewList.observe(viewLifecycleOwner) {
             adapter.submitList(it)
